fa55c57066556e0a4a614db8d3bec90a367cb089
Zanub <zanub.khan@yahoo.com>
2016-11-29 12:50:10 +0100

    i appplication

---
+++ ProCP  app/ProCP/Classes/Plot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCP.Classes
+{
+    class Plot
+    {
+        public int PlotId;
+        //private SoilTtype sotype;
+        private int PlotSize;
+
+      //  public Plot(SoilTypr sotype, int PlotSize) { }
+        public void setPlotsize(int sqmeters) { }
+        public int getPlotsize() { return 0; }
+     //   public bool AddCrop(Crop crop) { return false; }
+     //   public void setSoilType( SoilType so) { }
+        public bool RemoveCrop(int getploposition) { return false; }
+        public bool RemoveAllCrop(List<int> getploposition) { return false; }
+        public CropData GetCurrentCropData() { return null; }
+        public CropData GetCropSummary() { return null; }
+        public CropData GetCropDataByDate(DateTime d) { return null; }
+        //Needs some explanation
+        public void DrawSelf() { }
+        public void NurishLand() { }
+        public void Manageweeks() { }
+        public void CalBeginToEnd() { }
+        public void CalCurrentDate() { }
+
+
+
+
+
+    }
+}
---
+++ ProCP  app/ProCP/Classes/Simulation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCP.Classes
+{
+    class Simulation
+    {  // private List<Plot> plots;
+        private DateTime beginDate;
+        public DateTime BeginDate { get; set; }
+        
+        private DateTime endDate;
+        public DateTime EndDate { get; set; }
+
+        private DateTime currentDate;
+        public DateTime CurrentDate { get; set; }
+
+        private string province;
+        public DateTime Province { get; set; }
+
+       // public Simulation() { plots = new List<Plot>(); }
+       
+        public void Run() { }
+        public void Stop() { }
+        public void Restart() { }
+        public void Seek() { }
+        public void SetFertilizer(string fer) { }
+        public void Setwatering(string water) { }
+        public void SetProvince(int sqmeters) { }
+     // public int getPlotPosition(int pictureboxid) { return 0;}
+        public void SetBeginDate(DateTime date) { }
+        public void SetEndDate(DateTime date) { }
+        public void SetCurrentDate() { }
+        private void InitialPlots() { }
+
+
+
+
+
+
+        
+        
+    }
+}
