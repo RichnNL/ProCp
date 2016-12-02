55b83c430d2b8f3cbdbbacfac11a4fbd85c48325
TsankoBg <tsankohadzhiev@gmail.com>
2016-12-02 14:34:32 +0100

    abcd

--- ProCP  app/ProCP/Classes/PlotWeek.cs
+++ ProCP  app/ProCP/Classes/PlotWeek.cs
@@ -8,7 +8,38 @@ namespace ProCP.Classes
 {
     class PlotWeek
     {
+        public bool isEmpty { get; set; }
+        public int WaterSaturation { get; set; }
+        public int SaturationLooseRate { get; set; }
+        public int SoilNutrition { get; set; }
+        public int MaximumSoilNutrition { get; set; }
+        private Weather weather;
+        private Crop crop;
 
+        // constructor
+        public  PlotWeek()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="crop"></param>
+        public void setCrop(Crop crop)
+        {
+           
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="crop"></param>
+        public void DeleteCrop(Crop crop)
+        {
+
+        }
 
     }
 }
